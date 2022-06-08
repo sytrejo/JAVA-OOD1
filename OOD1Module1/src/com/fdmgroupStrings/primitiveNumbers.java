924bcb763809547d1b93225410dbd49390fec896
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-08 17:50:55 -0400

    working with primitive data types

---
+++ OOD1Module1/src/com/fdmgroupStrings/primitiveNumbers.java
@@ -0,0 +1,55 @@
+package com.fdmgroupStrings;
+
+public class primitiveNumbers {
+	public static void main(String[] args) {
+	
+	//Favorite Number
+	int favoriteNumber = 21;
+	
+	//print out my favorite number
+	System.out.println("My favorite number is" + favoriteNumber);
+	
+	//adding ints
+	int num1 = 5;
+	int num2 = 3;
+	
+	//add and print out the sum
+	System.out.println(num1 + num2);
+	
+	//arithmetic
+	//create an int variable called counter and give it the value 0
+	
+	int counter = 0;
+	
+	//increase the value of counter
+	counter ++;
+	
+	//add 5 to counter
+	counter +=5;
+	
+	//Decrement counter by 1
+	counter --;
+	
+	//Subtract 2 from counter
+	counter -=2;
+	
+	//Double counter's value
+	counter *=2;
+	
+	//Display the value of counter
+	System.out.println(counter);
+	
+	//Create int variables
+	int a = 5;
+	int b = 2;
+	double c = 5;
+	double d = 2;
+	
+	//Dividing ints and doubles
+	System.out.println(a/b);
+	System.out.println(c/d);
+	System.out.println(c/b);
+	System.out.println(a/d);
+	
+	
+	}}
